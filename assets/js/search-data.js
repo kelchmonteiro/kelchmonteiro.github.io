63416a8ed7ccdd4fcb921df0e3d0a500d6a09dd1
kelchmonteiro <74846498+kelchmonteiro@users.noreply.github.com>
2025-03-06 22:22:29 +0000

    Deploying to gh-pages from @ kelchmonteiro/kelchmonteiro.github.io@0a049af8e28e6ea4cf98fb32c4765e317fa6e1ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
+  },{id: "nav-repositories",
           title: "repositories",
           description: "The most used or studied repositories till now.",
           section: "Navigation",
